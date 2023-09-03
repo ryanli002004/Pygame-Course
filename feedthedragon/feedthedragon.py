b427da6d3ae82cce4ee2aad9f85f5a8a2fddbd4a
ryanli002004 <ryanli002004@gmail.com>
2023-09-03 10:36:47 -0400

    commit

---
+++ feedthedragon/feedthedragon.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+WINDOWW = 1000
+WINDOWH = 400
+display_surface = pygame.display.set_mode((WINDOWW, WINDOWH))
+pygame.display.set_caption("Feed the Dragon")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
